73cb6e091da2e010bc17c0229f938abba42097a9
gary-burke <garylewisburke@gmail.com>
2025-08-24 17:26:28 +0200

    Add function to assign blank-class to cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get the card elements and assign them to array "cards"
     let cards = document.getElementsByClassName("card");
 
+    /**
+     * Assign blank card class to cover content of cards.
+     * This creates the illusion that the cards have a front and back side.
+     * Cards need to be turned around in the physical game.
+     */
+    for (i of cards) {
+        i.classList.add("card-blank");
+    }
+
     // Add event listener to each card when clicked
     for (i of cards) {
         i.addEventListener("click", game);
