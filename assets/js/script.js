79b45b3af53e125d98f4d54a61242b99e429467f
gary-burke <garylewisburke@gmail.com>
2025-08-24 17:25:45 +0200

    Add event listener for DOM and cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,14 @@
 let currentScore = 0;
 let bestScore = 0;
 
+// Wait for the DOM to load before executing functions
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the card elements and assign them to array "cards"
+    let cards = document.getElementsByClassName("card");
 
+    // Add event listener to each card when clicked
+    for (i of cards) {
+        i.addEventListener("click", game);
+    }
+
+})
